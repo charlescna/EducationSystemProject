e5e52c039b49d98658b21c2b4865edd51ff1c6f9
charlescna <122284631+charlescna@users.noreply.github.com>
2023-12-08 14:14:00 -0600

    Testing complete. Everything works

--- src/main/java/com/example/educationsystemproject/Student.java
+++ src/main/java/com/example/educationsystemproject/Student.java
@@ -31,7 +31,6 @@ public class Student {
         this.phone = phone;
     }
 
-
     public Integer getStudentId() {
         return studentId;
     }
--- src/main/java/com/example/educationsystemproject/StudentRepository.java
+++ src/main/java/com/example/educationsystemproject/StudentRepository.java
@@ -2,8 +2,6 @@ package com.example.educationsystemproject;
 
 import org.springframework.data.repository.CrudRepository;
 
-// import com.example.educationsystemproject.Student;
-
 public interface StudentRepository extends CrudRepository<Student, Integer> {
 
 }
